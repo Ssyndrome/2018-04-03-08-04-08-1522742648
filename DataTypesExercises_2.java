942564ebe3d1e297eac233513097f194b1170855
915448715@qq.com <915448715@qq.com>
2018-07-21 14:46:51 +0800

    DataTypesExercises

---
+++ DataTypesExercises_2.java
@@ -0,0 +1,12 @@
+public class DataTypesExercises_2 {
+    public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a value for inch: ");
+        double inch = input.nextDouble();
+        double meters = inch * 0.0254;
+        System.out.println(inch + " inch is " + meters + " meters");
+
+    }
+}
